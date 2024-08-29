bd484ae64479135be9c6f971eb711b60dd4638f5
spaceofnova <mcrealms23@gmail.com>
2024-08-28 21:12:09 -0500

    1

--- src/components/ui/ModeToggle.tsx
+++ src/components/ui/ModeToggle.tsx
@@ -13,36 +13,46 @@ import {
 } from "@/components/ui/dropdown-menu";
 
 export function ModeToggle(props: { big?: boolean }) {
-  const { theme, setTheme } = useTheme();
+  const [isMounted, setisMounted] = React.useState(false);
+  const { setTheme } = useTheme();
+
+  React.useEffect(() => {
+    setisMounted(true);
+    return () => {
+      setisMounted(false);
+    };
+  }, []);
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        {props.big ? (
-          <Button
-            variant="outline"
-            className="flex items-center justify-center gap-2"
-          >
-            <span>Change theme</span>
-          </Button>
-        ) : (
-          <Button variant="ghost" size={"icon"}>
-            <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-            <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          </Button>
-        )}
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    isMounted && (
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          {props.big ? (
+            <Button
+              variant="outline"
+              className="flex items-center justify-center gap-2"
+            >
+              <span>Change theme</span>
+            </Button>
+          ) : (
+            <Button variant="ghost" size={"icon"}>
+              <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+              <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+            </Button>
+          )}
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem onClick={() => setTheme("light")}>
+            Light
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setTheme("dark")}>
+            Dark
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setTheme("system")}>
+            System
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    )
   );
 }
